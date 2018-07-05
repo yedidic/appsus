7ec5b157b10bd250f20e2ad6c62bf9b4d439de3a
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2018-07-05 14:00:42 +0300

    go back btn

--- js/pages/notes-app-cmp.js
+++ js/pages/notes-app-cmp.js
@@ -4,7 +4,14 @@ export default {
     name: 'email',
     template: `
     <section class="notes-app">
+        <button @click="goBack">Back to Appsus</button>
         <h1>Notes APp down here</h1>
     </section>
-    `
+    `,
+    methods: {
+        goBack() {
+			this.$router.push('/');
+		}
+    }
+
 }
