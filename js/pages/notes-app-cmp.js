e9a6b942b3429d48ff8e7b039d4d7f8025242354
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2018-07-05 14:35:04 +0300

    cpms folders

--- js/pages/notes-app-cmp.js
+++ js/pages/notes-app-cmp.js
@@ -1,4 +1,4 @@
-
+import notesService from '../services/notes-service.js'
 
 export default {
     name: 'email',
@@ -6,12 +6,38 @@ export default {
     <section class="notes-app">
         <button @click="goBack">Back to Appsus</button>
         <h1>Notes APp down here</h1>
+        <ul>
+            <li v-for="note in notes">
+            <h4>{{note.title}}</h4>
+            {{note.text}}
+            </li>
+        </ul>
+        <!-- <component v-for:"note in notes" :is="note"></component> -->
     </section>
     `,
+    data() {
+        return {
+            notes: []
+        }
+    },
+    created() {
+        notesService.query()
+        .then((data) => {
+            this.notes = data;
+            console.log(this.notes);
+        })  
+    },
     methods: {
         goBack() {
 			this.$router.push('/');
 		}
+    },
+    components: {
+        // textNote,
+        // imgNote,
+        // todoNote,
+        // audioNote,
+        // mapNote
     }
 
 }
--- js/services/notes-service.js
+++ js/services/notes-service.js
@@ -1,1 +1,14 @@
-console.log('notes service');
+
+var notes = [
+    {type: 'text', title:"Dream 05/07/18", text: 'I dreamt I finished sprint 3 and then a monster with 3 heads ate me'},
+    {type: 'text', title:"Dream 05/07/18", text: 'I dreamt I finished sprint 3 and then a monster with 3 heads ate me'},
+]
+
+function query() {
+    console.log(notes);
+    return Promise.resolve(notes);
+}
+
+export default {
+    query
+}
