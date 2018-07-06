95c9ade649f2b7bb70a9ce9eea53ac9ba44eb0e3
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2018-07-06 14:43:08 +0300

    todo note add edit

--- js/cmps/notes/edit-add-text-cmp.js
+++ js/cmps/notes/edit-add-text-cmp.js
@@ -8,11 +8,27 @@ export default {
         <div class="edit-add-input">
             <input v-model="note.data.title"></input>
             <textarea v-model="note.data.text"></textarea>
-            <img v-if="note.data.imgUrl" :src="note.data.imgUrl"/>
+            <img v-if="note.data.imgUrl && !imageInputShown" :src="note.data.imgUrl"/>
+            <canvas v-if="uploadedImg" ref="canvas"/>
+            <!-- <div v-if="imageInputShown" class="flex"> -->
+                <picture-input v-if="imageInputShown" 
+                                ref="pictureInput"
+                                width="400" 
+                                height="300" 
+                                :crop="false"
+                                margin="16" 
+                                accept="image/jpeg,image/png" 
+                                size="5" 
+                                button-class="btn" >
+                </picture-input>
+                <!-- <input type="text" v-model="enteredImgUrl" placeholder="Enter an image url"> -->
+                <!-- <input type="file" @change="onFileUpload" @click="getReadyForUpload"> -->
+                    <!-- <i class="fas fa-file-upload"></i> -->
+                <!-- </input> -->
+            </div>
+            <i class="far fa-image" @click="imageInputShown = true"></i>
         </div>
         <input type="color" value="AB8BA8" v-model="note.bgc">
-        <!-- <input class="jscolor" v-model="note.bgc"> -->
-        <!-- <jscolor v-model="note.bgc"></jscolor> -->
         <button @click="saveNote">Save</button>
 
         <pre>
@@ -32,18 +48,30 @@ export default {
     data() {
         return {
             note: null,
+            imageInputShown: false,
+            enteredImgUrl: '',
+            uploadedImg: null
             // data: this.data
         }
     },
     methods: {
+        // thingClicked() {
+        //     console.log('click!');
+        // },
         goBack() {
             this.$router.push('/notes');
         },
         saveNote() {
+            if (this.$refs.pictureInput) {
+                this.note.data.imgUrl = this.$refs.pictureInput.image
+            }
             notesService.saveNote(this.note)
                 .then(() => {
                     this.$router.push('/notes');
                 })
         }
     },
+    components: {
+        'picture-input': PictureInput
+      }
 }
--- js/routes.js
+++ js/routes.js
@@ -5,7 +5,7 @@ import appsusPage from './pages/appsus-page-cmp.js'
 import emailApp from './pages/email-app-cmp.js'
 import notesApp from './pages/notes-app-cmp.js'
 import editAddTxtCmp from './cmps/notes/edit-add-text-cmp.js'
-// import editAddTodoCmp from './cmps/notes/edit-add-todo-cmp.js'
+import editAddTodoCmp from './cmps/notes/edit-add-todo-cmp.js'
 
 // Export
 
@@ -16,5 +16,5 @@ export default [
     {path: '/email', component: emailApp},
     {path: '/notes', component: notesApp},
     {path: '/notes/edit-add/text/:noteId?', component: editAddTxtCmp},
-    // {path: '/notes/edit-add/todo/:id?', component: editAddTodoCmp},
+    {path: '/notes/edit-add/todo/:noteId?', component: editAddTodoCmp},
 ]
